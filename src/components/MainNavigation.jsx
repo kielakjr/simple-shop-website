74fad1344074b0ef6112c503e429ba223bcf2938
mkielak <kielakowsky17@gmail.com>
2026-01-16 21:05:38 +0100

    Added styling

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -2,9 +2,32 @@ import React from 'react'
 
 const MainNavigation = () => {
   return (
-    <header className="flex items-center justify-between p-4 bg-blue-600 text-white">
-      <h1>Simple Shop</h1>
-      <input type="search" placeholder="Search products..." />
+    <header className="
+      flex items-center justify-between p-4 rounded-2xl
+      backdrop-blur-xl
+      border border-white/20
+      text-white
+      shadow-[0_8px_30px_rgb(0,0,0,0.12)]
+      bg-black/30
+      m-4
+    ">
+      <h1 className="font-semibold text-lg">Simple Shop</h1>
+      <input
+        type="search"
+        placeholder="Search products..."
+        className="
+          px-3 py-1.5 rounded-md
+          bg-white/30 text-white placeholder-white/70
+          outline-none
+          focus:ring-2 focus:ring-white/50
+          border border-white/20
+          backdrop-blur-md
+          shadow-[0_4px_15px_rgb(0,0,0,0.1)]
+          transition-all duration-200
+          w-96
+        "
+      />
+
       <p>Cart: 0 items</p>
     </header>
   )
--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import MainNavigation from '../components/MainNavigation'
 
+import Background from '../components/ui/Background';
+
 const RootLayout = () => {
   return (
     <>
+      <Background />
       <MainNavigation />
       <main>
         <Outlet />
